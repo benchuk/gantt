d961ac79ae2d7241814be78633c9a9dc0e896e8d
benchuk <benchuk@gmail.com>
2019-03-11 23:34:41 +0200

    move to js and setup testing server

---
+++ server.js
@@ -0,0 +1,51 @@
+// content of index.js
+const http = require('http')
+const fs = require('fs');
+const port = 6001
+
+
+const requestHandler = (request, response) => {
+    console.log(request.url)
+    if (request.url === "/") {
+        console.log('handle index')
+        fs.readFile('./index2.html', function (err, html) {
+
+            response.writeHeader(200, {
+                "Content-Type": "text/html"
+            });
+            response.write(html);
+            response.end();
+        });
+        return;
+    }
+    console.log('handle other')
+    console.log('=======================================')
+    try {
+        fs.readFile(request.url.replace('/', "./"), function (err, js) {
+            if (err) {
+                console.log(err)
+                response.write("not found");
+                response.end();
+                return;
+            }
+            response.writeHeader(200, {
+                "Content-Type": "text/javascript"
+            });
+            response.write(js);
+            response.end();
+        });
+    } catch (e) {
+
+    }
+
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+
+    console.log(`server is listening on ${port}`)
+})
