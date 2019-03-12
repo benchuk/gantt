039bea65dd77ad35e6937fd51103a795bf8d60e5
benchuk <benchuk@gmail.com>
2019-03-12 08:41:20 +0200

    .

--- ganttLayoutManager.js
+++ ganttLayoutManager.js
@@ -12,15 +12,29 @@ function refresh() {
 
         var itemsCountInGroup = this.adapter.getCountItemsInDateRangeForGroupAt(groupIndex);
         for (var i = 0; i < itemsCountInGroup; i++) {
+            //task can be any graphics it wants
+            //task interface should be 
+            //setPosition(x,y)
+            //getHeight
+            //width will be rendered depending on the number of days the task takes and the view mode
+            //if task in group are taking place in the same time they will be rendered beneath each other
+            //else they will be rendered one after the other
             var task = this.adapter.CreateTaskFor(groupIndex, itemIndex);
             //TBD: Calc x,y
             task.setPosition(x, y)
+            task.setWidth() //task should hide text for example of too small
+            //h to calc next y
+            var h = task.getHeight();
         }
     }
 }
 
-//day, 3days, workdays, week, month, 3 month, year.
-function setViewType(type) {
+function onTaskAtIndexOfGroupDataChanged(groupIndex, itemIndex, startDate, endDate, height) {
+
+}
+
+//'day', '3Days', 'workDays', 'fullWeek', 'month', '3Month', 'year'.
+function setViewMode(mode) {
 
 }
 
